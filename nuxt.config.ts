5e4cedb435afe248adebc74af8a91dc72e5c9474
ryanadhi <ryanadhi.p@gmail.com>
2024-12-19 17:27:47 +0700

    feat: web improvements

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,24 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: "MoovieTime",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        {
+          charset: "utf-8",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: "MoovieTime - A movie app built with Nuxt.js",
+        },
+      ],
       link: [
         {
           rel: "stylesheet",
